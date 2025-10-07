8462b18f4364b3527cd8492884b1ea79a25f7ac6
brayan <brayansamsungfame@gmail.com>
2025-10-06 23:08:30 -0400

    trayendocambios

--- config/urls_tenant.py
+++ config/urls_tenant.py
@@ -6,10 +6,7 @@ router = DefaultRouter()
 
 urlpatterns = [
     path("api/", include(router.urls)),
-<<<<<<< HEAD
     path('api/hoteles/', include('apps.hoteles.urls')),
     path('api/habitaciones/', include('apps.habitaciones.urls')),
-=======
     path("", include("apps.usuarios.urls")),
->>>>>>> 6f8bfe4b2419cf590364055582ac229333123b19
 ]
