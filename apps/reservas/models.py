e9b43cf6a0fbb540e7b9cf71cdccc27757314e55
Gerson <villarroelpoma2@gmail.com>
2025-10-08 17:48:32 -0400

    Creacion de los modelos para las reservas

---
+++ apps/reservas/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+from apps.habitaciones.models import Habitacion
+from apps.hoteles.models import Hotel
+from apps.usuarios.models import User
+
+
+# Create your models here.
+class Reserva(models.Model):
+    CONFIRMADA = 'confirmada'
+    CANCELADA = 'cancelada'
+    PENDIENTE = 'pendiente'
+
+    ESTADO_CHOICES = [
+        (CONFIRMADA, 'Confirmada'),
+        (CANCELADA, 'Cancelada'),
+        (PENDIENTE, 'Pendiente'),
+    ]
+
+    fecha_reserva = models.DateField(auto_now_add=True)
+    fecha_entrada = models.DateField(null=False, blank=False)
+    fecha_salida = models.DateField(null=False, blank=False)
+    total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        blank=False
+    )
+    estado = models.CharField(
+        max_length=50,
+        choices=ESTADO_CHOICES,
+        default=PENDIENTE
+    ) # e.g., 'confirmada', 'cancelada', etc.
+    huesped = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reservas'
+    )
+    hotel = models.ForeignKey(
+        Hotel,
+        on_delete=models.CASCADE,
+        related_name='reservas'
+    )
+
+class DetalleReserva(models.Model):
+    reserva = models.ForeignKey(
+        Reserva,
+        on_delete=models.CASCADE,
+        related_name='habitaciones_reservadas'
+    )
+    habitacion = models.ForeignKey(
+        Habitacion,
+        on_delete=models.CASCADE,
+        related_name='reservas'
+    )
+
+    class Meta:
+        unique_together = ('reserva', 'habitacion')
+
+class ServicioExtra(models.Model):
+    servicio = models.ForeignKey(
+        'servicios.Servicio',
+        on_delete=models.CASCADE,
+        related_name='servicios_extra'
+    )
+    detalle_reserva = models.ForeignKey(
+        DetalleReserva,
+        on_delete=models.CASCADE,
+        related_name='servicios_extra'
+    )
+    cantidad = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('servicio', 'detalle_reserva')
