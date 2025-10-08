4cf42ce0d0f2ecdd00185b7e14aa98dc77fc1aaa
brayan <brayansamsungfame@gmail.com>
2025-10-08 01:26:23 -0400

    ya resuelto, no molesten

--- config/urls_tenant.py
+++ config/urls_tenant.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('api/', include('apps.hoteles.urls')),
     path("", include("apps.usuarios.urls")),
+    # path('api/hoteles/', include('apps.hoteles.urls')),
     path('api/habitaciones/', include('apps.habitaciones.urls')),
 ]
 
