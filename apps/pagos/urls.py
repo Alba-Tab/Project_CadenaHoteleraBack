8f3f439e60d6b097a504c723d0ed827982f22c6c
Taddy Micheld Riveros Perez <taddymicheldriverosperez@gmail.com>
2025-10-19 18:24:46 -0400

    mis cambios

--- apps/pagos/urls.py
+++ apps/pagos/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PagoViewSet
 
 router = DefaultRouter()
-router.register(r'pagos', PagoViewSet, basename='pago')
+router.register(r'', PagoViewSet, basename='pago')
 
 urlpatterns = [
     path('', include(router.urls)),
