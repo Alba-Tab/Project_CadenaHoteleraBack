37ba57e2b0b61aea5ffca9fce10d264eb805f60f
Brayan Albaro Garcia Taboada <garcia.brayan3001@gmail.com>
2025-10-21 13:21:38 -0400

    quitar puerto innecesario

--- core/services.py
+++ core/services.py
@@ -48,7 +48,7 @@ class TenantFormService:
         Hola {username},
 
         Se ha creado tu tenant {tenant.name}.
-        Accede en: http://{full_domain}:4200/authentication/login
+        Accede en: http://{full_domain}/authentication/login
         tus credenciales son las siguientes:
         Usuario: {username}
         Contraseña: {password}
