c0443db463cf9c79f868f90fb734b463d8ccd3f7
Brayan Albaro Garcia Taboada <110486695+Alba-Tab@users.noreply.github.com>
2025-11-18 02:22:46 -0400

    Update Python command to use python3

--- apprunner-start.sh
+++ apprunner-start.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Migraciones para django-tenants
-python manage.py migrate_schemas --shared
-python manage.py migrate_schemas --tenant
+python3 manage.py migrate_schemas --shared
+python3 manage.py migrate_schemas --tenant
 
 # Static
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Arrancar Gunicorn
 gunicorn --bind 0.0.0.0:${PORT:-8000} config.wsgi:application
