c9e778ec7fc43001deff98c1095a6c24bcadb2be
Brayan Albaro Garcia Taboada <110486695+Alba-Tab@users.noreply.github.com>
2025-11-18 09:13:27 -0400

    Update apprunner-start.sh

--- apprunner-start.sh
+++ apprunner-start.sh
@@ -6,7 +6,7 @@ python3 manage.py migrate_schemas --shared
 python3 manage.py migrate_schemas --tenant
 
 # Static
-python3 manage.py collectstatic --noinput
+#python3 manage.py collectstatic --noinput
 
 # Arrancar Gunicorn
 gunicorn --bind 0.0.0.0:${PORT:-8000} config.wsgi:application
