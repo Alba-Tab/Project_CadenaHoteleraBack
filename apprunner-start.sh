383a3ab602813ffa250e4b517e28b8e7d33c3873
Brayan Albaro Garcia Taboada <110486695+Alba-Tab@users.noreply.github.com>
2025-11-18 10:23:45 -0400

    Update apprunner-start.sh

--- apprunner-start.sh
+++ apprunner-start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "=== DIAGNÓSTICO DE PAQUETES INSTALADOS ==="
-pip list
+echo "=== DIAGNÓSTICO RÁPIDO ==="
+# Usamos pip3 explícitamente
+pip3 list | grep Django
 echo "=== FIN DIAGNÓSTICO ==="
-# Migraciones para django-tenants
+
+# Migraciones (usando python3)
 python3 manage.py migrate_schemas --shared
 python3 manage.py migrate_schemas --tenant
 
-# Static
-#python3 manage.py collectstatic --noinput
+# Estáticos (Descomenta esto cuando arregles el STATIC_ROOT en settings.py)
+# python3 manage.py collectstatic --noinput
 
-# Arrancar Gunicorn
+# Arrancar Gunicorn (usando python3 -m para evitar error de ruta)
+echo "=== INICIANDO SERVIDOR ==="
 python3 -m gunicorn --bind 0.0.0.0:${PORT:-8000} config.wsgi:application
