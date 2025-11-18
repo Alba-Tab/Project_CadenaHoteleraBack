8812ee1e4d2eaf856ee79099ee8e7bb8b14e1781
Brayan Albaro Garcia Taboada <110486695+Alba-Tab@users.noreply.github.com>
2025-11-18 11:24:25 -0400

    Update apprunner-start.sh

--- apprunner-start.sh
+++ apprunner-start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "=== INICIANDO SCRIPT ==="
-# Verificamos si Django llegó vivo
+echo "=== VERIFICANDO INSTALACION ==="
+# Ahora esto SÍ debería mostrar Django
 pip3 list | grep Django
 
 # Migraciones
 python3 manage.py migrate_schemas --shared
 python3 manage.py migrate_schemas --tenant
 
-# Arrancar Gunicorn
+# Arrancar Gunicorn (COMO MÓDULO)
 echo "=== INICIANDO SERVIDOR ==="
 python3 -m gunicorn --bind 0.0.0.0:${PORT:-8000} config.wsgi:application
